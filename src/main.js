8e0a9079b0593fa26f134bd72bd116bc0fa1cdb7
bluesky <blueskyisblue@outlook.com>
2024-05-01 13:13:54 +0800

    Fix 404 problem

--- src/main.js
+++ src/main.js
@@ -3,12 +3,8 @@ import "./assets/main.css";
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
-import mitt from "mitt";
 
 const app = createApp(App);
 app.use(router);
-
-const eventBus = mitt();
-app.provide("eventBus", eventBus);
 
 app.mount("#app");
